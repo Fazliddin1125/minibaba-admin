e580cfce39dd3ad50bda8f68eb41e4bba1b741c2
Abdubakiyev <abdubakiyevabdulloh798@gmail.com>
2026-03-04 09:52:27 +0500

    Test: order boldi 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products";
--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
   const { t, i18n } = useTranslation();
 
   return (
-    <header className="w-full  h-[8vh] bg-white border-b border-gray-100 px-4 md:px-6 flex items-center justify-center fixed top-0 z-50 font-sans">
+    <header className="w-full h-[8vh] bg-white border-b border-gray-100 px-4 md:px-6 flex items-center justify-center font-sans">
 
       <div className="w-full  flex items-center justify-between  mx-auto">
         {/* Chap tomondagi blok */}
